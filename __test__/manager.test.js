e33130193a7038d0f985f33a328b11f1c10dcf4a
avidrunner87 <36962520+avidrunner87@users.noreply.github.com>
2021-07-28 22:59:20 -0500

    Created tests for manager

--- __test__/manager.test.js
+++ __test__/manager.test.js
@@ -3,28 +3,28 @@ const Manager = require('../lib/manager');
 
 describe("Manager Class", () => {
     it("Can be Instantiated", () => {
-        const intern = new Intern(1, 'Dave', 'dave@hotmail.com', 'University of Development');
-        expect(typeof (intern)).toBe("object");
-        expect(intern instanceof Intern).toBe(true);
+        const manager = new Manager(1, 'Dave', 'dave@hotmail.com', 2342);
+        expect(typeof (manager)).toBe("object");
+        expect(manager instanceof Manager).toBe(true);
     })
 
-    it('Should Have a School Property That Can Be Set on Instantiation', () => {
-        const intern = new Intern(1, 'Dave', 'dave@hotmail.com', 'University of Development');
-        expect(intern.school).toBe('University of Development');
+    it('Should Have an Office Number Property That Can Be Set on Instantiation', () => {
+        const manager = new Manager(1, 'Dave', 'dave@hotmail.com', 2342);
+        expect(manager.officeNumber).toBe(2342);
     })
 
     it('Should Have an Id Property That Can Be Set on Instantiation', () => {
-        const intern = new Intern(1, 'Dave', 'dave@hotmail.com', 'University of Development');
-        expect(intern.id).toBe(1);
+        const manager = new Manager(1, 'Dave', 'dave@hotmail.com', 2342);
+        expect(manager.id).toBe(1);
     })
 
-    it('Should Have a Function to Get the School That Can Be Called After Instantiation', () => {
-        const intern = new Intern(1, 'Dave', 'dave@hotmail.com', 'University of Development');
-        expect(intern.getSchool(1)).toBe('University of Development');
+    it('Should Have a Function to Get the Office Number That Can Be Called After Instantiation', () => {
+        const manager = new Manager(1, 'Dave', 'dave@hotmail.com', 2342);
+        expect(manager.getOfficeNum(1)).toBe(2342);
     })
 
     it('Should Have a Function to Get the Role That Can Be Called After Instantiation', () => {
-        const intern = new Intern(1, 'Dave', 'dave@hotmail.com', 'University of Development');
-        expect(intern.getRole(1)).toBe('Intern');
+        const manager = new Manager(1, 'Dave', 'dave@hotmail.com', 2342);
+        expect(manager.getRole(1)).toBe('Manager');
     })
 });
